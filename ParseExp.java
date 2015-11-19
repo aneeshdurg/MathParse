3711e1e586cbb6dacc2a5b24b12942c18e1d195c
aneeshdurg <durg2@illinois.edu>
2015-11-19 10:18:14 -0600

    Changed the way the expression is parsed and stored in the object. Added some methods.

--- ParseExp.java
+++ ParseExp.java
@@ -4,25 +4,35 @@ class ParseExp
 	private String exp;
 	private String ops = "^*/+-";
 	private int counter=0;
-	public ParseExp(String exp)
+	private int numpointer=0;
+	private int oppointer=0;
+	private	LinkedList<Double> numbers = new LinkedList<Double>();
+	private LinkedList<Character> operations = new LinkedList<Character>();
+	public ParseExp(String exp) throws ParseException
 	{
+		if (left(exp)!=right(exp))
+			throw new ParseException("Unmatched brackets!");
 		exp = exp.replaceAll("\\s+", "").toLowerCase();
 		this.exp = exp;
+		parse();
 	}
-	private ParseExp(String exp, int counter)
+	private ParseExp(String exp, int counter) throws ParseException
 	{
+		if (left(exp)!=right(exp))
+			throw new ParseException("Unmatched brackets!");		
 		exp = exp.replaceAll("\\s+", "").toLowerCase();
 		this.exp = exp;
 		this.counter = counter;
+		parse();
 	}
-	public double parse() throws ParseException
+	private void parse() throws ParseException
 	{
+		String exp = this.exp;
 		int length = exp.length();
-		LinkedList<Double> numbers = new LinkedList<Double>();
-		LinkedList<Character> operations = new LinkedList<Character>();
 		double currentnum=0;
 		boolean decimal=false;
 		int decimalcount=0;
+		boolean neg=false;
 		while(exp.length()>0)
 		{
 			char now = exp.charAt(0);	
@@ -44,11 +54,20 @@ class ParseExp
 					currentnum+=(now-48)*Math.pow(10, 0-decimalcount);
 					decimalcount++;
 				}
+
 				exp = exp.substring(1, exp.length());
+
 				if (exp.length()==0)
+				{
+					if(neg)
+					{
+						currentnum=0-currentnum;
+					}
 					numbers.add(currentnum);
+				}
 				counter++;
 			}
+			//handles decimal numbers
 			else if(now=='.')
 			{
 				decimal=true;
@@ -64,49 +83,86 @@ class ParseExp
 				if(next<=57&&next>=48)
 					throw new ParseException("Error at "+counter+":"+length+"! Please check and ensure that the expression is properly formatted!");
 				currentnum = Math.PI;
+				if (exp.equals("pi"))
+					numbers.add(currentnum);
 				exp = exp.substring(2, exp.length());
 				counter++;
 			}
-
 			//Expression inside ()
 			else if(now=='(')
 			{
-				if (exp.indexOf(")")<1)
+				//need to check for nested ) so that index of doesn't pick it up?
+				if (exp.lastIndexOf(")")<1)
 					throw new ParseException("Error at "+counter+":"+length+"! Please check and ensure that the expression is properly formatted!");
-				String subexp = exp.substring(exp.indexOf("(")+1, exp.indexOf(")"));
-				ParseExp brackets = new ParseExp(subexp, counter);
-				currentnum=brackets.parse();
-				counter+=subexp.length()+2;
-				exp = exp.substring(exp.indexOf(")")+1, exp.length());
+				if (exp.indexOf("-)")==1)
+				{
+					neg=true;
+					counter+=3;
+					exp = exp.substring(exp.lastIndexOf(")")+1, exp.length());
+				}
+				else
+				{
+					int closing=1;
+					for(int i=1; i<exp.length(); i++)
+					{
+						if (exp.charAt(i)=='(') closing++;
+						else if (exp.charAt(i)==')') closing--;
+						if (closing==0) 
+						{
+							closing=i;
+							i=exp.length();
+						}
+					}
+					String subexp = exp.substring(exp.indexOf("(")+1, closing);
+					ParseExp brackets = new ParseExp(subexp, counter);
+					currentnum=brackets.evaluate();
+					counter+=subexp.length()+2;
+					exp = exp.substring(closing+1, exp.length());
+					if (exp.length()==0)
+						numbers.add(currentnum);
+				}
 			}
-
+			//adds operators
 			else if(ops.contains(now+""))
 			{
+				if(neg)
+				{
+					currentnum=0-currentnum;
+					neg=false;
+				}
 				operations.add(now);
 				numbers.add(currentnum);
 				exp = exp.substring(1, exp.length());
 				currentnum = 0;
-				counter++;
-
-				
+				counter++;	
 			}
-
+			//Illegal characters
 			if((next<48||next>57)&&!ops.contains(now+"")&&!ops.contains(next+"")&&next!='.'&&next!=0&&!(now=='p'&&next=='i'))
 				throw new ParseException("Error at "+counter+":"+length+"! Please check and ensure that the expression is properly formatted!");
 		}
-		
+		//Illegal operators
 		if(numbers.size()!=operations.size()+1)
 		{
 			throw new ParseException("Operations ("+operations.size()+") and arguments ("+numbers.size()+") do not match! Please check and ensure that the expression is properly formatted!");	
 		}
+	}
 
+	public double evaluate() throws ParseException
+	{
+		LinkedList<Double> numbers = new LinkedList<Double>();
+		LinkedList<Character> operations = new LinkedList<Character>();
+		for(int i=0; i<this.numbers.size()-1; i++)
+		{
+			numbers.add(this.numbers.get(i));
+			operations.add(this.operations.get(i));
+		}
+		numbers.add(this.numbers.get(this.numbers.size()-1));
 		//loops through operations in the order ^*/+-
 		for(int o=0; o<ops.length(); o++)
 		{
 			while(operations.contains(ops.charAt(o)))
 			{
 				int i = operations.indexOf(ops.charAt(o));
-
 				if (ops.charAt(o)=='^')
 					numbers.add(i, Math.pow(numbers.get(i), numbers.get(i+1)));
 				else if(ops.charAt(o)=='*')
@@ -117,7 +173,7 @@ class ParseExp
 					numbers.add(i, numbers.get(i)+numbers.get(i+1));
 				else if(ops.charAt(o)=='-')
 					numbers.add(i, numbers.get(i)-numbers.get(i+1));
-
+				//updates the operators and numbers
 				numbers.remove(i+1);
 				numbers.remove(i+1);
 				operations.remove(i);
@@ -126,9 +182,64 @@ class ParseExp
 
 		return numbers.get(0);
 	}
-	public static double parse(String exp) throws ParseException
+	public static double evaluate(String exp) throws ParseException
 	{
-		return new ParseExp(exp).parse();
+		return new ParseExp(exp).evaluate();
+	}
+
+	public double nextNum() throws ParseException
+	{
+		if (numpointer>=numbers.size()) 
+			throw new ParseException("No doubles found!");
+		numpointer++;
+		return numbers.get(numpointer);
+	}
+	public boolean hasNextNum()
+	{
+		return numpointer<numbers.size();
+	}
+
+	public double nextOp() throws ParseException
+	{
+		if (oppointer>=operations.size()) 
+			throw new ParseException("No doubles found!");
+		oppointer++;
+		return operations.get(oppointer);
+	}
+	public boolean hasNextOp()
+	{
+		return oppointer<operations.size();
+	}
+
+	public String getExp()
+	{
+		return exp;
+	}
+	//counts the number of (
+	private int left(String s)
+	{
+		int counter = 0;
+		for( int i=0; i<s.length(); i++ ) 
+		{
+    		if( s.charAt(i) == '(' ) 
+    		{
+        		counter++;
+    		}	 
+		}
+		return counter;
+	}
+	//counts the number of )
+	private int right(String s)
+	{
+		int counter = 0;
+		for( int i=0; i<s.length(); i++ ) 
+		{
+    		if( s.charAt(i) == ')' ) 
+    		{
+        		counter++;
+    		}	 
+		}
+		return counter;
 	}
 }
 class ParseException extends Exception
--- README.md
+++ README.md
@@ -1,9 +1,28 @@
 # ParseExp
 	A class used to parse a String into a mathematical expression.
 
-	double parse()
+	double evaluate()
 		Parses the string which the ParseExp object was initiallized 
 		with.
 
-	double parse(String exp)
+	double evaluate(String exp)
 		Parses the string passed to the function
+
+	double nextNum()
+		Returns the next number in the expression. Throws ParseException
+		if no number is found. Use hasNextNum to avoid this.
+
+	boolean hasNextNum()
+		Returns true if there is another number in the expression, false
+		otherwise.
+
+	double nextOp()
+		Returns the next operation in the experssion. Throws ParseException
+		if no operation is found. Use hasNextOp to avoid this.	
+
+	boolean hasNextOp()
+		Returns true if there is another operation in the expression, false
+		otherwise.
+
+	String getExp()
+		Returns the original expression as a String.	
